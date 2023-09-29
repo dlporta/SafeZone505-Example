9f87dae40bbc7daa731bdcb6e5dfa080ec78674b
dlporta <david.hernandez.rivera.2002@gmail.com>
2023-09-29 17:42:14 -0600

    base updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/SafeZone505-Example/', // Cambia 'nombre-de-repo' por el nombre de tu repositorio
+  base: '/SafeZone505-Example/SafeZone505-Example/', // Cambia 'nombre-de-repo' por el nombre de tu repositorio
 });
